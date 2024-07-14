12c46ed9e480db31fce1397d2f6a5498e377aeaf
Rizvana12 <kmrizvana3@gmail.com>
2024-07-14 16:44:05 +0530

    first commit

---
+++ Day1/index.js
@@ -0,0 +1,37 @@
+//Activity1:varible declaration
+   //Task1
+var num=5;
+console.log(num);
+   //Task2
+let str="Hello there";
+console.log(str);
+//Activity2:Constant declaration
+   //Task3
+const bool=true;
+console.log(bool);
+//Activity3:Data Types
+   //Task4
+let number=4;
+let string="Hey";
+let obj={
+    length:1
+}     
+let array=[1,2,3];
+
+console.log(typeof (number));
+console.log(typeof (string));
+console.log(typeof (obj));
+console.log(typeof (array));
+
+//Activity4:Reassigning variable
+   //Task5
+let randomVar=3;
+console.log(randomVar);
+randomVar="Hey";
+console.log(randomVar);
+
+//Activity5:Understanding const
+    //Task6
+const checkConst=6;
+  checkConst=8;
+console.log(checkConst);      
---
+++ Day2/index.js
@@ -0,0 +1,40 @@
+//Arithmetic Operations
+let a=10;
+let b=7;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Assignment Operators
+
+let c=6;
+c+=1;
+console.log(c);
+c-=5;
+console.log(c);
+
+//Comparison operator
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a===b);
+
+//Logical Operator
+
+let k=3;
+let l=5;
+let m=8;
+let n=10;
+
+console.log(k>l&&m>n);
+console.log(k>l||m>n);
+console.log(!k);
+
+//Ternary Operator
+
+console.log(k>0?"Positive":"Negative");
